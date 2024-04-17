1bdaa5919df67a5fdb7de0d3146ef2a5b1754fec
mathfm <fernandomath.mds@gmail.com>
2024-04-17 16:48:13 -0300

    feat: header

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import "../App.css"
+export default function Header() {
+    return (
+        <header className="d-flex flex-wrap justify-content-center p-3 mb-4 border-bottom background-header">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        
+        <span className="fs-4">Health Plus</span>
+      </a>
+
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+      </ul>
+    </header>
+    )
+}
