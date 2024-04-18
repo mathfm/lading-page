b2aeda65b78023064b097012a07e215891f164c4
João Targino <joaoetargino@gmail.com>
2024-04-18 18:21:35 -0300

    feat: add card component and update app.jsx with new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
 import Home from './components/home'
 import Header from './components/header'
+import CardSpecialities from './components/card'
+import heart from './assets/Frame2.svg'
+import esporte from './assets/esporte.svg'
+import bemestar from './assets/bem-estar.svg'
+import Footer from './components/footer'
 
 function App() {
   return (
-     <>
+    <>
       <Header />
       <Home />
+      <div className="services">
+        <h3>Nossos serviços</h3>
+        <div className='division-bar bg-primary'></div>
+        <p>Conheça os serviços disponibilizados pela HealthPlus:</p>
+
+        <div className="cards">
+          <CardSpecialities image={heart} title="Saúde" text="Melhore sua saúde com o HealthPlus!" />
+          <CardSpecialities image={esporte} title="Esporte" text="Adquira saúde por meio dos esportes com o HealthPlus!" />
+          <CardSpecialities image={bemestar} title="Bem Estar" text="Além da saúde física, cuide da sua saúde mental com o HealthPlus!" />
+        </div>
+      </div>
+      <br />
+      <Footer/>
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
-import Card from "react-bootstrap/Card";
-
-function HeaderAndFooterExample({ title, image, text }) {
+function CardSpecialities({ image, title, text }) {
   return (
-    <Card className="card ">
-      <Card.Img variant="top" src={image} className="card-image" />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
-      </Card.Body>
+    <Card className="d-flex align-items-center justify-content-center" style={{ height: '40vh', width: '18vw', boxShadow: '4px 4px 6px 0px rgba(0, 0, 0, 0.5)', padding: '5px', border: 'none'}}>
+      <div className="text-center">
+        <Card.Img src={image} style={{ width: "82px" }} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {text}
+          </Card.Text>
+        </Card.Body>
+      </div>
     </Card>
   );
 }
 
-export default HeaderAndFooterExample;
+export default CardSpecialities;
