d2bb9df15a22563fc3b4139b09c4bc661767da55
mathfm <fernandomath.mds@gmail.com>
2024-04-22 16:52:58 -0300

    Refactor: add line

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,7 @@ export default function Footer() {
             <p className="text-center text-white">Participantes do projeto</p>
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><a href="https://github.com/gabriel-augg" className="nav-link px-2 text-white" target="_blank" rel="noopener noreferrer">gabriel-augg</a></li>
+                <li className="nav-item"><a href="https://github.com/mathfm" className="nav-link px-2 text-white" target="_blank" rel="noopener noreferrer">mathfm</a></li>
                 <li className="nav-item"><a href="https://github.com/AlpharJorge" className="nav-link px-2 text-white" target="_blank" rel="noopener noreferrer">alpharjorge</a></li>
                 <li className="nav-item"><a href="https://github.com/Gleison-dev" className="nav-link px-2 text-white" target="_blank" rel="noopener noreferrer">gleison-dev</a></li>
                 <li className="nav-item"><a href="https://github.com/JonasLeiteProgramador" className="nav-link px-2 text-white" target="_blank" rel="noopener noreferrer">jonasleiteprogramador</a></li>
